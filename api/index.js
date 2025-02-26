a241bfb7a043391d2e4c0826de788756477a859b
RodrigoAzvdd <rodrigo.azvd750@gmail.com>
2025-02-26 12:32:37 -0300

    update PORT

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
-// api/index.js
 const express = require('express');
 const app = express();
 
+// Vercel automaticamente define a variável de ambiente `PORT`
+const port = process.env.PORT || 3000; // Porta 3000 como fallback
+
 app.get('/', (req, res) => {
-    res.send('Hello, Vercel!');
+    res.send('Hello from Express on Vercel!');
 });
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
 
 module.exports = app;
