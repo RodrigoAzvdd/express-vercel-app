24983c2a5ff9642172736c1bb29535604ebb430f
RodrigoAzvdd <rodrigo.azvd750@gmail.com>
2025-02-26 12:56:56 -0300

    update cors policy

--- api/index.js
+++ api/index.js
@@ -1,16 +1,61 @@
 const express = require('express');
 const app = express();
 
-// Vercel automaticamente define a variável de ambiente `PORT`
-const port = process.env.PORT || 3000; // Porta 3000 como fallback
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // Responder às requisições OPTIONS (preflight)
+    if (req.method === 'OPTIONS') {
+        return res.status(200).send();
+    }
+
+    next();
+});
+
+
+// Importante: adicionar middleware para analisar JSON no corpo da requisição
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello from Express on Vercel!');
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+app.post('/ncm', (req, res) => {
+    const { codigo } = req.body; // Usando req.query em vez de req.body para GET requests
 
+    if (!codigo) {
+        return res.json({ valido: false, mensagem: "Código NCM não fornecido" });
+    }
+
+    fetch('https://portalunico.siscomex.gov.br/classif/api/publico/nomenclatura/download/json')
+        .then(response => response.json())
+        .then(data => {
+            // Verificar se o NCM existe nos dados
+            const ncmValido = data.Nomenclaturas.find(item => item.Codigo === String(codigo));
+
+            if (ncmValido) {
+                res.json({
+                    valido: true,
+                    mensagem: "NCM válido",
+                    descricao: ncmValido.Descricao || "Sem descrição disponível"
+                });
+            } else {
+                res.json({
+                    valido: false,
+                    mensagem: "NCM inválido"
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao buscar o JSON:', error);
+            res.status(500).json({
+                valido: false,
+                mensagem: "Erro ao verificar NCM",
+                erro: error.message
+            });
+        });
+});
 
 module.exports = app;
