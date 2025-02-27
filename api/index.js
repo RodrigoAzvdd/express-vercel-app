31fc8437a5501b67c95c7f3f594229ea937cfeee
RodrigoAzvdd <rodrigo.azvd750@gmail.com>
2025-02-27 08:09:47 -0300

    update server config

--- api/index.js
+++ api/index.js
@@ -397,10 +397,10 @@ app.get('/ncm/:codigo', NcmController.getNcmByCodeParam);
 // Exportar o app para a Vercel
 module.exports = app;
 
-// Se precisar iniciar em ambiente local
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Servidor rodando na porta ${PORT}`);
-    });
-}
+// // Se precisar iniciar em ambiente local
+// if (process.env.NODE_ENV !== 'production') {
+//     const PORT = process.env.PORT || 3000;
+//     app.listen(PORT, () => {
+//         console.log(`Servidor rodando na porta ${PORT}`);
+//     });
+// }
